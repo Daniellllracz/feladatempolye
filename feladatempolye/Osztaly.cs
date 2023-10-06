30602e16be7308161389c2bd20cded35e87e8084
Dániel Rácz <dracz898@gmail.com>
2023-10-06 11:17:57 +0200

    Add project files.

---
+++ feladatempolye/Osztaly.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladatempolye
+{
+    class Osztaly
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string Gender { get; set; }
+        public string MarielStatus { get; set; }
+        public double Salery { get; set; }
+
+
+        public Osztaly(string sor, string name, int age, string city, string department, string position, string gender, string marielStatus, int salery)
+        {
+            string[] szortir = sor.Split(';');
+            Name = name;
+            Age = age;
+            City = city;
+            Department = department;
+            Position = position;
+            Gender = gender;
+            MarielStatus = marielStatus;
+            Salery = salery;
+
+        }
+        public override string ToString() 
+        {
+            return $"{Name}név | {Age}életkor  | {City}Város  | {Department}department  | {Position}pozícó | {Gender}nem | {MarielStatus} családi állapott | {Salery} bevétel";
+        }
+    }
+}
---
+++ feladatempolye/ProgramBase.cs
@@ -0,0 +1,32 @@
+﻿namespace feladatempolye
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            //1.Készíts egy osztályt, amely tartalmazza a szükséges mezőket. Nem kötelező kidolgozni a property - ket.
+
+            //2.Írd meg a konstruktort.
+
+            //3.Készíts egy osztályon belüli virtuális metódust az adatok kiírására.
+
+            //4.Propertyk kidolgozása(Szorgalmi feladat)
+            //Dolgozd ki a property-ket is, és használd őket az adatokhoz való korrekt hozzáférésre és módosításra.
+
+            //5.Hibakezelés(Szorgalmi feladat)
+            //Implementálj hibakezelést az alkalmazásban, például az adatok beolvasásakor vagy a fájlba írás során.
+
+            //6.Az osztály segítségével hozz létre egy listát, amely objektumpéldányokat tartalmaz a forrásfájlból beolvasott adatokkal.
+            var adatok = new List<Osztaly>();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\Adatok.txt",
+                encoding: System.Text.Encoding.UTF8);
+
+            while (!sr.EndOfStreamException)
+            {
+                public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
